e0a4d82968ec6d2f0949314f7b580c797027644d
Aadil Minhaz <aadilminhaz@Aadils-MacBook-Air.local>
2024-03-12 01:39:28 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import FlitWelcome from './components/flitWelcome.js'
+
+
+/**function FlitWelcome() {
+  return(
+    <div className="welcome">
+      <h1>Welcome to Flit AI.</h1>
+      <p>Tell me a date, I will make you flit from one best place to another.</p>
+    </div>
+  );
+}*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <FlitWelcome/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +30,15 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
+      
     </div>
+    
+    
   );
 }
+
+
 
 export default App;
---
+++ src/components/flitWelcome.js
@@ -0,0 +1,10 @@
+function FlitWelcome() {
+    return(
+      <div className="welcome">
+        <h1>Welcome to Flit AI.</h1>
+        <p>Tell me a date, I will make you flit from one best place to another.</p>
+      </div>
+    );
+  }
+
+  export default FlitWelcome;
